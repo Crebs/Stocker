47c2baa0516b488f9f8eb70c0712c92b57979261
Riley Crebs <rileycrebs@gmail.com>
2020-02-06 08:59:21 -0800

    Adding pandas to create dataframe

--- soup_scraper.py
+++ soup_scraper.py
@@ -12,9 +12,30 @@ driver.get(quote_page)
 content = driver.page_source
 soup = BeautifulSoup(content, 'lxml')
 
-roic_box = soup.find('th', attrs={'id': 'i27'})
+# Get Date columns
+profit_header = soup.find('th', attrs={'id': 'pr-profit'})
+# print (profit_header.text)
+# Collect Dates
+dates = []
+for sibling in profit_header.next_siblings:
+    dates.append(sibling.text)
 
-print (roic_box.text)
+# Return on Invested Capital
+roic_box = soup.find('th', attrs={'id': 'i27'})
+# print (roic_box.text)
+# Collect Values
+values = []
 for child in roic_box.parent.findAll('td'):
-    print (child.text)
+    values.append(child.text)
+
+# initialise data of lists.
+data = {profit_header.text:values}
+
+# Creates pandas DataFrame.
+df = pd.DataFrame(data, index =dates)
+
+# print the data
+print (df)
+
+# Clean up
 driver.quit()
