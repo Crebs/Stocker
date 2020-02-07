45b0a826720752c270e503c90a8654238e9440a6
Riley Crebs <rileycrebs@gmail.com>
2020-02-06 22:27:22 -0800

    added code to calculate intrinsic value

--- soup_scraper.py
+++ soup_scraper.py
@@ -14,7 +14,6 @@ soup = BeautifulSoup(content, 'lxml')
 
 # Get Date columns
 profit_header = soup.find('th', attrs={'id': 'pr-profit'})
-# print (profit_header.text)
 # Collect Dates
 dates = []
 for sibling in profit_header.next_siblings:
@@ -22,20 +21,53 @@ for sibling in profit_header.next_siblings:
 
 # Return on Invested Capital
 roic_box = soup.find('th', attrs={'id': 'i27'})
-# print (roic_box.text)
 # Collect Values
 values = []
 for child in roic_box.parent.findAll('td'):
     values.append(child.text)
 
+# Free Cash Flow Per Share
+free_cash_flow_per_share_header = soup.find('th', attrs={'id': 'i90'})
+print (free_cash_flow_per_share_header.text)
+cf_values = []
+for child in free_cash_flow_per_share_header.parent.findAll('td'):
+    cf_values.append(child.text)
+
 # initialise data of lists.
-data = {profit_header.text:values}
+data = {roic_box.text:values, free_cash_flow_per_share_header.text:cf_values}
 
 # Creates pandas DataFrame.
 df = pd.DataFrame(data, index =dates)
 
 # print the data
 print (df)
+
+cf_range = df.iloc[[-7,-2],[0]]
+
+cf_past = float(cf_range.iat[0,0])
+cf_recent = float(cf_range.iat[-1,0])
+annual_growth_rate = ((cf_recent/cf_past)**(1/5))-1
+# print (annual_growth_rate)
+
+f_values = []
+for i in range(1,6):
+    f_values.append(cf_recent*(1+annual_growth_rate)**i)
+# print (f_values)
+
+discout_rate = 0.1
+d_cf = 0
+for i in range(1,6):
+    d_cf += f_values[i-1]/((1+discout_rate)**i)
+# print (d_cf)
+
+growth_rate = 0.03
+terminal_value = (f_values[-1]*(1+growth_rate))/(discout_rate-growth_rate)
+# print(terminal_value)
+
+num_of_years = 5
+intrinsic_value = (terminal_value+d_cf)/(1+discout_rate)**num_of_years
+print(intrinsic_value)
+
 
 # Clean up
 driver.quit()
