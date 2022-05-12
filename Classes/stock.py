98eddc70964acc9c197d4e63a26e466f3ddaaf86
Riley <crebs@me.com>
2022-05-12 20:07:47 +0000

    Update intrinsic value code

--- Classes/stock.py
+++ Classes/stock.py
@@ -57,30 +57,42 @@ class Stock(object):
     def __intrinsic_value(self):
         try:
             # Calcualte Intrinsic Value of the stock
-            cf_range = self.df.iloc[[-6,-2],:]["FCFPS"]  # free cash flow per share
-            cf_past = cf_range.iloc[0]
+            number_years = 5 # this is up to preference, but most recomend 5 years
+            cf_range = self.df.iloc[-6:-1]["FCFPS"]  # free cash flow per share
+            cf_past_index = 0
+            cf_past = cf_range.iloc[cf_past_index]
             cf_recent = cf_range.iloc[-1]
+
+            while(cf_past <= 0 and number_years > 1):
+                print("Update Range")
+                cf_past_index += 1
+                number_years -= 1
+                cf_past = cf_range.iloc[cf_past_index]
+                print("new past value: " + str(cf_past))
+                print("range of years: " + str(number_years))
+
             if float(cf_past) <= 0 or float(cf_recent) <= 0:
                 print("No valid range, skipping intrinsic value calculation")
                 return 0
         
-            annual_growth_rate = ((cf_recent/cf_past)**(1.0/5.0))-1.0
+            annual_growth_rate = ((cf_recent/cf_past)**(1.0/number_years))-1.0
             f_values = []
-            for i in range(1,6):
+            for i in range(1,number_years+1):
                 f_values.append(cf_recent*(1+annual_growth_rate)**i)
             # 10% rate of return is the more normal
             discout_rate = 0.1
             d_cf = 0
-            for i in range(1,6):
+            for i in range(1,number_years+1):
                 d_cf += f_values[i-1]/((1+discout_rate)**i)
             # Normal Rate of GDP is about 3%
             growth_rate = 0.03
             terminal_value = (f_values[-1]*(1+growth_rate))/(discout_rate-growth_rate)
-            num_of_years = 5
-            intrinsic_value = (terminal_value+d_cf)/(1+discout_rate)**num_of_years
+            intrinsic_value = (terminal_value+d_cf)/(1+discout_rate)**number_years
         except Exception as e:
             intrinsic_value = 0
         return intrinsic_value
+
+
 
     def __quote_from_disk(self):
         try:
